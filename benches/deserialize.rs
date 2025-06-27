24971940f33c77b8aefbd53b3f797ddb051c48dc
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-06-26 23:44:12 -0700

    Use `SliceCursor` in serialization and add deserialization benchmarks
    Massively lowers the standard benchmarks since we're writing to the stack, from about 20ns to about 4ns.

--- benches/deserialize.rs
+++ benches/deserialize.rs
@@ -1,3 +1,142 @@
 //! Benchmarks for basic type deserialization.
 
+use divan::{bench, black_box};
+use facet_minecraft::{Deserializer, McDeserializer};
+
 fn main() { divan::main() }
+
+#[bench(args = [
+    [0u8].as_slice(),
+    [127u8].as_slice(),
+    [255u8].as_slice()
+])]
+fn deserialize_u8(input: &[u8]) {
+    let (de, rem) = McDeserializer.deserialize_u8(black_box(input)).unwrap();
+    debug_assert!(matches!(de, 0 | 127 | 255), "Expected u8 values 0, 127, or 255, got: {de}");
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+#[bench(args = [
+    [0u8, 0u8].as_slice(),
+    [255u8, 0u8].as_slice(),
+    [255u8, 255u8].as_slice()
+])]
+fn deserialize_u16(input: &[u8]) {
+    let (de, rem) = McDeserializer.deserialize_u16(black_box(input)).unwrap();
+    debug_assert!(
+        matches!(de, 0 | 255 | 65535),
+        "Expected u16 values 0, 32767, or 65534, got: {de}"
+    );
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+#[bench(args = [
+    [0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8].as_slice()
+])]
+fn deserialize_u32(input: &[u8]) {
+    let (de, rem) = McDeserializer.deserialize_u32(black_box(input)).unwrap();
+    debug_assert!(
+        matches!(de, 0 | 65535 | 4294967295),
+        "Expected u32 values 0, 65535, or 4294967295, got: {de}"
+    );
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+#[bench(args = [
+    [0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8, 0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8].as_slice()
+])]
+fn deserialize_u64(input: &[u8]) {
+    let (de, rem) = McDeserializer.deserialize_u64(black_box(input)).unwrap();
+    debug_assert!(
+        matches!(de, 0 | 4294967295 | 18446744073709551615),
+        "Expected u64 values 0, 4294967295, or 18446744073709551615, got: {de}"
+    );
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+#[bench(args = [
+    [0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8].as_slice()
+])]
+fn deserialize_u128(input: &[u8]) {
+    let (de, rem) = McDeserializer.deserialize_u128(black_box(input)).unwrap();
+    debug_assert!(
+        matches!(de, 0 | 18446744073709551615 | 340282366920938463463374607431768211455),
+        "Expected u128 values 0, 18446744073709551615, or 340282366920938463463374607431768211455, got: {de}"
+    );
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+#[bench(args = [
+    [0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8].as_slice()
+])]
+#[cfg(target_pointer_width = "32")]
+fn deserialize_usize(input: &[u8]) {
+    let (de, rem) = McDeserializer.deserialize_usize(black_box(input)).unwrap();
+    debug_assert!(
+        matches!(de, 0 | 65535 | 4294967295),
+        "Expected u32 values 0, 65535, or 4294967295, got: {de}"
+    );
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+#[bench(args = [
+    [0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8, 0u8, 0u8, 0u8, 0u8].as_slice(),
+    [255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8, 255u8].as_slice()
+])]
+#[cfg(target_pointer_width = "64")]
+fn deserialize_usize(input: &[u8]) {
+    let (de, rem) = McDeserializer.deserialize_usize(black_box(input)).unwrap();
+    debug_assert!(
+        matches!(de, 0 | 4294967295 | 18446744073709551615),
+        "Expected u64 values 0, 4294967295, or 18446744073709551615, got: {de}"
+    );
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+// -------------------------------------------------------------------------------------------------
+
+// #[bench(args = [])]
+// fn deserialize_var_u16(input: &[u8]) {}
+
+// #[bench(args = [])]
+// fn deserialize_var_u32(input: &[u8]) {}
+
+// #[bench(args = [])]
+// fn deserialize_var_u64(input: &[u8]) {}
+
+// #[bench(args = [])]
+// fn deserialize_var_u128(input: &[u8]) {}
+
+// #[bench(args = [])]
+// fn deserialize_var_usize(input: &[u8]) {}
+
+// -------------------------------------------------------------------------------------------------
+
+#[bench(args = [
+    [0u8].as_slice(),
+    [1u8].as_slice()
+])]
+fn deserialize_bool(input: &[u8]) {
+    let (_de, rem) = McDeserializer.deserialize_bool(black_box(input)).unwrap();
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
+
+#[bench(args = [
+    [0u8].as_slice(),
+    [3u8, b'a', b'b', b'c'].as_slice(),
+    [3u8, b'1', b'2', b'3'].as_slice(),
+    [13u8, b'H', b'e', b'l', b'l', b'o', b',', b' ', b'w', b'o', b'r', b'l', b'd', b'!'].as_slice()
+])]
+fn deserialize_str(input: &[u8]) {
+    let (_de, rem) = McDeserializer.deserialize_str(black_box(input)).unwrap();
+    debug_assert!(rem.is_empty(), "Expected no remaining bytes, got: {rem:?}");
+}
--- benches/serialize.rs
+++ benches/serialize.rs
@@ -1,101 +1,103 @@
 //! Benchmarks for basic type serialization.
 
 use divan::{bench, black_box};
-use facet_minecraft::{McSerializer, Serializer, SerializerExt};
+use facet_minecraft::{McSerializer, Serializer, SerializerExt, SliceCursor};
 
 fn main() { divan::main() }
 
+const SLICE_SIZE: usize = 32;
+
 #[bench(args = [u8::MIN, u8::MIN + u8::MAX / 2, u8::MAX])]
-fn serialize_u8(val: u8) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(1));
+fn serialize_u8(val: u8) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_u8(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [u16::MIN, u16::MIN + u16::MAX / 2, u16::MAX])]
-fn serialize_u16(val: u16) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(2));
+fn serialize_u16(val: u16) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_u16(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [u32::MIN, u32::MIN + u32::MAX / 2, u32::MAX])]
-fn serialize_u32(val: u32) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(4));
+fn serialize_u32(val: u32) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_u32(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [u64::MIN, u64::MIN + u64::MAX / 2, u64::MAX])]
-fn serialize_u64(val: u64) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(8));
+fn serialize_u64(val: u64) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_u64(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [u128::MIN, u128::MIN + u128::MAX / 2, u128::MAX])]
-fn serialize_u128(val: u128) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(16));
+fn serialize_u128(val: u128) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_u128(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [usize::MIN, usize::MIN + usize::MAX / 2, usize::MAX])]
-fn serialize_usize(val: usize) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(8));
+fn serialize_usize(val: usize) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_usize(black_box(val)).unwrap();
-    ser.0
 }
 
 // -------------------------------------------------------------------------------------------------
 
 #[bench(args = [u16::MIN, u16::MIN + u16::MAX / 2, u16::MAX])]
-fn serialize_var_u16(val: u16) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(3));
+fn serialize_var_u16(val: u16) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_var_u16(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [u32::MIN, u32::MIN + u32::MAX / 2, u32::MAX])]
-fn serialize_var_u32(val: u32) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(5));
+fn serialize_var_u32(val: u32) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_var_u32(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [u64::MIN, u64::MIN + u64::MAX / 2, u64::MAX])]
-fn serialize_var_u64(val: u64) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(10));
+fn serialize_var_u64(val: u64) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_var_u64(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [u128::MIN, u128::MIN + u128::MAX / 2, u128::MAX])]
-fn serialize_var_u128(val: u128) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(19));
+fn serialize_var_u128(val: u128) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_var_u128(black_box(val)).unwrap();
-    ser.0
 }
 
 #[bench(args = [usize::MIN, usize::MIN + usize::MAX / 2, usize::MAX])]
-fn serialize_var_usize(val: usize) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(10));
+fn serialize_var_usize(val: usize) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_var_usize(black_box(val)).unwrap();
-    ser.0
 }
 
 // -------------------------------------------------------------------------------------------------
 
 #[bench(args = [true, false])]
-fn serialize_bool(val: bool) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(1));
+fn serialize_bool(val: bool) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_bool(black_box(val)).unwrap();
-    ser.0
 }
 
-#[bench(args = ["", "abc", "123", "Hello, world!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor..."])]
-fn serialize_str(val: &str) -> Vec<u8> {
-    let mut ser = McSerializer(Vec::with_capacity(val.len() + 2));
+#[bench(args = ["", "abc", "123", "Hello, world!"])]
+fn serialize_str(val: &str) {
+    let mut slice = [0u8; SLICE_SIZE];
+    let mut ser = McSerializer(SliceCursor::new(&mut slice));
     ser.serialize_str(black_box(val)).unwrap();
-    ser.0
 }
