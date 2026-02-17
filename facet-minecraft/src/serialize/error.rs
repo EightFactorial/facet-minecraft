45ea56a57067051629caf5f259cf0ebdbb670e8d
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2026-02-16 18:55:31 -0800

    chore: WIP Serializer

--- facet-minecraft/src/serialize/error.rs
+++ facet-minecraft/src/serialize/error.rs
@@ -1,7 +1,11 @@
+use alloc::borrow::Cow;
 use core::{
     error::Error,
     fmt::{self, Display, Formatter},
 };
+
+use facet::{Facet, Shape};
+use facet_format::ScalarValue;
 
 /// An error that occurred during serialization.
 #[derive(Debug)]
@@ -10,6 +14,48 @@ pub struct SerializeError {
 }
 
 impl SerializeError {
+    /// Create a new [`SerializeError`].
+    #[must_use]
+    pub const fn new(kind: SerializeErrorKind) -> Self { Self { kind } }
+
+    /// Create a new [`SerializeError`] for an unsupported type.
+    #[inline]
+    #[must_use]
+    pub const fn unsupported_type<'a, T: Facet<'a>>() -> Self { Self::unsupported_shape(T::SHAPE) }
+
+    /// Create a new [`SerializeError`] for an unsupported shape.
+    #[inline]
+    #[must_use]
+    pub const fn unsupported_shape(shape: &'static Shape) -> Self {
+        Self::new(SerializeErrorKind::UnsupportedType(shape))
+    }
+
+    /// Create a new [`SerializeError`] for an attempt to variable-length
+    /// serialize an unsupported type.
+    #[inline]
+    #[must_use]
+    pub const fn variable_length(shape: &Shape) -> Self {
+        Self::new(SerializeErrorKind::VariableLength(shape.type_identifier))
+    }
+
+    /// Create a new [`SerializeError`] for an attempt to variable-length
+    /// serialize a [`ScalarValue`] that does not support it.
+    #[must_use]
+    pub const fn variable_length_scalar(scalar: &ScalarValue) -> Self {
+        match scalar {
+            ScalarValue::Unit | ScalarValue::Null => Self::variable_length(<()>::SHAPE),
+            ScalarValue::Bool(_) => Self::variable_length(bool::SHAPE),
+            ScalarValue::Char(_) => Self::variable_length(char::SHAPE),
+            ScalarValue::I64(_) => Self::variable_length(i64::SHAPE),
+            ScalarValue::U64(_) => Self::variable_length(u64::SHAPE),
+            ScalarValue::I128(_) => Self::variable_length(i128::SHAPE),
+            ScalarValue::U128(_) => Self::variable_length(u128::SHAPE),
+            ScalarValue::F64(_) => Self::variable_length(f64::SHAPE),
+            ScalarValue::Str(_) => Self::variable_length(Cow::<str>::SHAPE),
+            ScalarValue::Bytes(_) => Self::variable_length(Cow::<[u8]>::SHAPE),
+        }
+    }
+
     /// Get the kind of serialization error.
     #[inline]
     #[must_use]
@@ -19,6 +65,13 @@ impl SerializeError {
 /// The type of serialization error.
 #[derive(Debug)]
 pub enum SerializeErrorKind {
+    /// An error occurred while writing to the buffer.
+    BufferError,
+    /// Attempted to serialize a type that is not supported.
+    UnsupportedType(&'static Shape),
+    /// Attempted to variable-length serialize a type that does not support it.
+    VariableLength(&'static str),
+
     /// An I/O error occurred.
     #[cfg(feature = "std")]
     Io(std::io::Error),
--- facet-minecraft/src/serialize/mod.rs
+++ facet-minecraft/src/serialize/mod.rs
@@ -1,9 +1,12 @@
 //! TODO
 
-use alloc::vec::Vec;
+use alloc::{borrow::Cow, vec::Vec};
 
-use facet::{Facet, Shape};
-use facet_format::{FieldOrdering, FormatSerializer, ScalarValue, SerializeError as FSError};
+use facet::{Facet, Shape, ShapeLayout};
+use facet_format::{
+    DynamicValueEncoding, DynamicValueTag, EnumVariantEncoding, FieldOrdering, FormatSerializer,
+    MapEncoding, ScalarValue, SerializeError as FSError, StructFieldMode,
+};
 use facet_reflect::{FieldItem, Peek};
 
 mod buffer;
@@ -72,51 +75,247 @@ impl
 /// A serializer that implements [`FormatSerializer`].
 pub struct McSerializer<'buffer, B: SerializeBuffer + ?Sized> {
     buffer: &'buffer mut B,
+    variable_length: bool,
+    value_size: usize,
 }
 
 impl<'buffer, B: SerializeBuffer + ?Sized> McSerializer<'buffer, B> {
     /// Create a new [`McSerializer`].
     #[inline]
     #[must_use]
-    pub const fn new(buffer: &'buffer mut B) -> Self { Self { buffer } }
+    pub const fn new(buffer: &'buffer mut B) -> Self {
+        Self { buffer, variable_length: false, value_size: 0 }
+    }
 
-    /// Consume the serializer and return the buffer reference.
+    /// Re-borrow the serializer with a shorter lifetime.
     #[inline]
     #[must_use]
-    pub fn into_inner(self) -> &'buffer mut B { self.buffer }
+    pub const fn reborrow<'a>(&'a mut self) -> McSerializer<'a, B> {
+        McSerializer {
+            buffer: self.buffer,
+            variable_length: self.variable_length,
+            value_size: self.value_size,
+        }
+    }
 
     /// Create a [`McSerializer`] over a
     /// [`dyn SerializeBuffer`](SerializeBuffer) from this serializer.
     #[inline]
     #[must_use]
-    pub const fn as_dyn<'a: 'buffer>(&'a mut self) -> McSerializer<'a, dyn SerializeBuffer + 'a>
+    pub const fn as_dyn<'a>(&'a mut self) -> McSerializer<'a, dyn SerializeBuffer + 'a>
     where
         B: Sized + 'a,
     {
-        McSerializer { buffer: self.buffer }
+        McSerializer {
+            buffer: self.buffer,
+            variable_length: self.variable_length,
+            value_size: self.value_size,
+        }
     }
+
+    /// Consume the serializer and return the buffer reference.
+    #[inline]
+    #[must_use]
+    pub const fn into_inner(self) -> &'buffer mut B { self.buffer }
 }
 
 impl<B: SerializeBuffer + ?Sized> FormatSerializer for McSerializer<'_, B> {
     type Error = SerializeError;
 
-    fn begin_struct(&mut self) -> Result<(), Self::Error> { todo!() }
+    fn struct_metadata(&mut self, shape: &Shape) -> Result<(), Self::Error> {
+        if self.variable_length && !shape.is_transparent() {
+            Err(SerializeError::variable_length(shape))
+        } else {
+            Ok(())
+        }
+    }
 
-    fn field_key(&mut self, _key: &str) -> Result<(), Self::Error> { todo!() }
+    fn begin_struct(&mut self) -> Result<(), Self::Error> { Ok(()) }
 
-    fn end_struct(&mut self) -> Result<(), Self::Error> { todo!() }
+    fn end_struct(&mut self) -> Result<(), Self::Error> { Ok(()) }
 
-    fn begin_seq(&mut self) -> Result<(), Self::Error> { todo!() }
+    fn begin_enum_variant(&mut self, index: usize, _: &'static str) -> Result<(), Self::Error> {
+        self.scalar_variable(ScalarValue::U64(index as u64), true)
+    }
 
-    fn end_seq(&mut self) -> Result<(), Self::Error> { todo!() }
+    fn field_metadata(&mut self, field: &FieldItem) -> Result<(), Self::Error> {
+        if let Some(field) = field.field.as_ref() {
+            if field.has_attr(Some("mc"), "variable") {
+                self.variable_length = true;
+            }
 
-    fn scalar(&mut self, _val: ScalarValue<'_>) -> Result<(), Self::Error> { todo!() }
+            if let ShapeLayout::Sized(layout) = field.shape().layout {
+                self.value_size = layout.size();
+            }
+        }
 
-    fn field_metadata(&mut self, _field: &FieldItem) -> Result<(), Self::Error> { todo!() }
+        Ok(())
+    }
 
-    fn struct_metadata(&mut self, _shape: &Shape) -> Result<(), Self::Error> { todo!() }
+    fn field_key(&mut self, _: &str) -> Result<(), Self::Error> { Ok(()) }
+
+    fn scalar(&mut self, val: ScalarValue<'_>) -> Result<(), Self::Error> {
+        let variable_length = core::mem::take(&mut self.variable_length);
+        self.scalar_variable(val, variable_length)
+    }
+
+    fn serialize_byte_sequence(&mut self, bytes: &[u8]) -> Result<bool, Self::Error> {
+        self.scalar_variable(ScalarValue::Bytes(Cow::Borrowed(bytes)), true)?;
+        Ok(true)
+    }
+
+    fn serialize_byte_array(&mut self, bytes: &[u8]) -> Result<bool, Self::Error> {
+        if self.buffer.extend_buffer(bytes) {
+            Ok(true)
+        } else {
+            Err(SerializeError::new(SerializeErrorKind::BufferError))
+        }
+    }
+
+    fn begin_seq_with_len(&mut self, len: usize) -> Result<(), Self::Error> {
+        self.scalar_variable(ScalarValue::U64(len as u64), true)
+    }
+
+    fn begin_seq(&mut self) -> Result<(), Self::Error> { Ok(()) }
+
+    fn end_seq(&mut self) -> Result<(), Self::Error> { Ok(()) }
+
+    fn begin_map_with_len(&mut self, len: usize) -> Result<(), Self::Error> {
+        self.scalar_variable(ScalarValue::U64(len as u64), true)
+    }
+
+    fn end_map(&mut self) -> Result<(), Self::Error> { Ok(()) }
+
+    fn begin_option_some(&mut self) -> Result<(), Self::Error> {
+        self.scalar_variable(ScalarValue::Bool(true), false)
+    }
+
+    fn serialize_none(&mut self) -> Result<(), Self::Error> {
+        self.scalar_variable(ScalarValue::Bool(false), false)
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    fn is_self_describing(&self) -> bool { false }
 
     fn preferred_field_order(&self) -> FieldOrdering { FieldOrdering::Declaration }
+
+    fn struct_field_mode(&self) -> StructFieldMode { StructFieldMode::Unnamed }
+
+    fn enum_variant_encoding(&self) -> EnumVariantEncoding { EnumVariantEncoding::Index }
+
+    fn map_encoding(&self) -> MapEncoding { MapEncoding::Pairs }
+
+    fn dynamic_value_encoding(&self) -> DynamicValueEncoding { DynamicValueEncoding::Tagged }
+
+    fn dynamic_value_tag(&mut self, _tag: DynamicValueTag) -> Result<(), Self::Error> { Ok(()) }
+}
+
+impl<B: SerializeBuffer + ?Sized> McSerializer<'_, B> {
+    fn scalar_variable(&mut self, val: ScalarValue, variable: bool) -> Result<(), SerializeError> {
+        if match (val, variable) {
+            (ScalarValue::Unit | ScalarValue::Null, false) => true,
+            (ScalarValue::Bool(v), false) => {
+                let byte = v as u8;
+                self.buffer.extend_buffer(&[byte])
+            }
+
+            (ScalarValue::I64(v), false) => {
+                let bytes = v.to_le_bytes();
+                self.buffer.extend_buffer(&bytes[..self.value_size])
+            }
+            (ScalarValue::U64(v), false) => {
+                let bytes = v.to_le_bytes();
+                self.buffer.extend_buffer(&bytes[..self.value_size])
+            }
+            (ScalarValue::I64(v), true) => {
+                let mut buffer = [0; _];
+                let len = Self::var_u64(v as u64, &mut buffer);
+                self.buffer.extend_buffer(&buffer[..len])
+            }
+            (ScalarValue::U64(v), true) => {
+                let mut buffer = [0; _];
+                let len = Self::var_u64(v, &mut buffer);
+                self.buffer.extend_buffer(&buffer[..len])
+            }
+
+            (ScalarValue::I128(v), false) => {
+                let bytes = v.to_le_bytes();
+                self.buffer.extend_buffer(&bytes[..self.value_size])
+            }
+            (ScalarValue::U128(v), false) => {
+                let bytes = v.to_le_bytes();
+                self.buffer.extend_buffer(&bytes[..self.value_size])
+            }
+            (ScalarValue::I128(v), true) => {
+                let mut buffer = [0; _];
+                let len = Self::var_u128(v as u128, &mut buffer);
+                self.buffer.extend_buffer(&buffer[..len])
+            }
+            (ScalarValue::U128(v), true) => {
+                let mut buffer = [0; _];
+                let len = Self::var_u128(v, &mut buffer);
+                self.buffer.extend_buffer(&buffer[..len])
+            }
+
+            (ScalarValue::F64(v), false) => {
+                let bytes = v.to_le_bytes();
+                self.buffer.extend_buffer(&bytes[..self.value_size])
+            }
+            (ScalarValue::Str(v), false) => {
+                let mut buffer = [0; _];
+                let len = Self::var_u64(v.len() as u64, &mut buffer);
+                self.buffer.extend_buffer(&buffer[..len]) && self.buffer.extend_buffer(v.as_bytes())
+            }
+            (ScalarValue::Bytes(v), false) => {
+                let mut buffer = [0; _];
+                let len = Self::var_u64(v.len() as u64, &mut buffer);
+                self.buffer.extend_buffer(&buffer[..len]) && self.buffer.extend_buffer(v.as_ref())
+            }
+
+            // Unsupported
+            (ScalarValue::Char(_), _) => {
+                return Err(SerializeError::unsupported_type::<char>());
+            }
+            (val, true) => {
+                return Err(SerializeError::variable_length_scalar(&val));
+            }
+        } {
+            Ok(())
+        } else {
+            Err(SerializeError::new(SerializeErrorKind::BufferError))
+        }
+    }
+
+    fn var_u64(mut v: u64, buf: &mut [u8; 10]) -> usize {
+        let mut byte;
+        let mut count = 0;
+        while (v != 0 || count == 0) && count < 10 {
+            byte = (v & 0b0111_1111) as u8;
+            v = (v >> 7) & (u64::MAX >> 6);
+            if v != 0 {
+                byte |= 0b1000_0000;
+            }
+            buf[count] = byte;
+            count += 1;
+        }
+        count
+    }
+
+    fn var_u128(mut v: u128, buf: &mut [u8; 19]) -> usize {
+        let mut byte;
+        let mut count = 0;
+        while (v != 0 || count == 0) && count < 19 {
+            byte = (v & 0b0111_1111) as u8;
+            v = (v >> 7) & (u128::MAX >> 6);
+            if v != 0 {
+                byte |= 0b1000_0000;
+            }
+            buf[count] = byte;
+            count += 1;
+        }
+        count
+    }
 }
 
 // -------------------------------------------------------------------------------------------------
